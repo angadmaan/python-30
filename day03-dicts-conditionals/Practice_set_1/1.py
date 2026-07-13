1f3b4a21aaeb7d787faf81dccd56fbbbcb075105
angadmaan <angadmaancse@gmail.com>
2026-07-14 00:14:36 +0530

    day 3

---
+++ day03-dicts-conditionals/Practice_set_1/1.py
@@ -0,0 +1,14 @@
+# Write a program to create a dictionary of hindi words with values as their english translation.abs
+
+words = { 
+    "madad" : "help",
+    "bhagwan" : "god",
+    "kripya" : "please",
+    "pyar" : "love",
+    "ghar" : "home"
+}
+
+word = input("Enter the word you want meaning of: ")
+
+print(words[word])
+
---
+++ day03-dicts-conditionals/Practice_set_1/3.py
@@ -0,0 +1,8 @@
+# What is the length of the set given below?
+
+s = set()
+s.add(20)
+s.add(20.0)
+s.add('20') # length of s after these operations?
+
+print(len(s))
