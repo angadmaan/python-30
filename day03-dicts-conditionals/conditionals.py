a1a25e29c6a827f5902b131315d4a79713468914
angadmaan <angadmaancse@gmail.com>
2026-07-15 23:13:18 +0530

    day 3

--- day03-dicts-conditionals/conditionals.py
+++ day03-dicts-conditionals/conditionals.py
@@ -1,4 +1,22 @@
-# Conditional Expression
+# Conditional Expressions in python
+
+
+a = int(input("Enter your age: "))
+
+if (a>=18):
+    print("\nYou are eligible for driving license")
+elif(a<=0):
+    print("\nEnter valid age")
+else:
+    print("\nYou are not eligible for driving license")
+
+
+print("Thank you for using the driving license eligibility checker") # This print is out of if-else ladder
+
+# Relational Operators in python 
+
+
+
 
 
 
