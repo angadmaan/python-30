cd92e72f8d909a90809786e0885e90e16efb68ea
angadmaan <angadmaancse@gmail.com>
2026-07-22 23:43:22 +0530

    day 4

--- day04-loops-functions/Practice_set_1/6.py
+++ day04-loops-functions/Practice_set_1/6.py
@@ -1,2 +1,6 @@
 # Write a program to print the multiplication table of a number using for loops in reversed order.
 
+n = int(input("Enter the number for which you want the multiplication table: "))
+
+for i in range(10, 0, -1):
+    print(f"{n} * {i} = {n * i}")
